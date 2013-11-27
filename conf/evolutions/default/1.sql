6714ab3d05658e7f5eb150b9f03a8f24d86afdb9
kugelmeister <kugelmeister@hotmail.com>
2013-11-27 13:48:56 +0000

    ev01

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -12,6 +12,14 @@ create table activity (
   constraint pk_activity primary key (id))
 ;
 
+create table location (
+  id                        bigint not null,
+  activity_id               bigint not null,
+  latitude                  float,
+  longitude                 float,
+  constraint pk_location primary key (id))
+;
+
 create table my_user (
   id                        bigint not null,
   firstname                 varchar(255),
@@ -23,10 +31,14 @@ create table my_user (
 
 create sequence activity_seq;
 
+create sequence location_seq;
+
 create sequence my_user_seq;
 
 alter table activity add constraint fk_activity_my_user_1 foreign key (user_id) references my_user (id);
 create index ix_activity_my_user_1 on activity (user_id);
+alter table location add constraint fk_location_activity_2 foreign key (activity_id) references activity (id);
+create index ix_location_activity_2 on location (activity_id);
 
 
 
@@ -34,9 +46,13 @@ create index ix_activity_my_user_1 on activity (user_id);
 
 drop table if exists activity cascade;
 
+drop table if exists location cascade;
+
 drop table if exists my_user cascade;
 
 drop sequence if exists activity_seq;
+
+drop sequence if exists location_seq;
 
 drop sequence if exists my_user_seq;
 
